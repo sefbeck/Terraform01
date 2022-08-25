7e748c2006fc8ee2e3bfb6c351bd31524f3bc298
sefbeck <abiolaoluwaseun31@gmail.com>
2022-08-25 00:55:18 -0400

    added new terraform

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "my_image" { 
+  type = string
+  default= "ami-02f3416038bdb17fb"
+}
+
+variable "my_type" {
+  type = string
+  default= "t2.micro"
+}
+
+variable "my_tag" {
+  type = string
+  default= "Afolly"
+}
